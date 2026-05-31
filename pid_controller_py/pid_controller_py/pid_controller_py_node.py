ef79ab838d98e7337d4f315d0798d12a28427b5e
Weeeeeeeeeeee <rubaiyatabdullahrabi71@gmail.com>
2026-05-31 23:03:32 +0600

    Added comments to python controller

--- pid_controller_py/pid_controller_py/pid_controller_py_node.py
+++ pid_controller_py/pid_controller_py/pid_controller_py_node.py
@@ -8,42 +8,42 @@ class PidControllerPyNode(Node):
     def __init__(self):
         super().__init__("pid_controller_py_node")
 
-        self.position = np.array([0.0, 0.0, 0.0])
-        self.velocity = np.array([0.0, 0.0, 0.0])
-        self.acceleration = np.array([0.0, 0.0, 0.0])
+        self.position = np.array([0.0, 0.0, 0.0])   #Plant current position
+        self.velocity = np.array([0.0, 0.0, 0.0])   #Plant current velocity
+        self.acceleration = np.array([0.0, 0.0, 0.0]) #Plant current acceleration
 
-        self.force = np.array([0.0, 0.0, 0.0])
+        self.force = np.array([0.0, 0.0, 0.0])      #The control input of PID
         self.omega = 0.5                        #Angular velocity for circling target
 
-        self.mass = 1.0
-        self.dt = 0.01
+        self.mass = 1.0     #Mass of plant
+        self.dt = 0.01      #Time interval of the loop
 
-        self.x = 0.0
-        self.y = 0.0
-        self.v_x = 0.0
-        self.v_y = 0.0
-        self.radius = 5.0
-        self.target_position = np.array([0.0, 0.0, 0.0])
-        self.target_velocity = np.array([0.0, 0.0, 0.0])
+        self.x = 0.0        #x axis position of target
+        self.y = 0.0        #y axis position of target
+        self.v_x = 0.0      #x axis velocity of target
+        self.v_y = 0.0      #y axis velocity of target
+        self.radius = 5.0   #radius of the circle created by the movement of plant
+        self.target_position = np.array([0.0, 0.0, 0.0])    #Target position (trajectory)
+        self.target_velocity = np.array([0.0, 0.0, 0.0])    #Target velocity (changing)
 
-        self.kp = np.array([2.0, 2.0, 2.0])
-        self.ki = np.array([0.5, 0.5, 1.0])
-        self.kd = np.array([0.2, 0.2, 0.6])
+        self.kp = np.array([2.0, 2.0, 2.0])     #Proportional constant or weight
+        self.ki = np.array([0.5, 0.5, 1.0])     #Integral constant or weight
+        self.kd = np.array([0.2, 0.2, 0.6])     #Derivative constant or weight
 
-        self.error = np.array([0.0, 0.0, 0.0])
+        self.error = np.array([0.0, 0.0, 0.0])  #Distance from target to plant current position
         self.velocity_error = np.array([0.0, 0.0, 0.0])         #Desired Velocity 
-        self.prev_error = np.array([0.0, 0.0, 0.0])
-        self.prev_velocity = np.array([0.0, 0.0, 0.0])
-        self.integral = np.array([0.0, 0.0, 0.0])
-        self.derivative = np.array([0.0, 0.0, 0.0])
+        self.prev_error = np.array([0.0, 0.0, 0.0])     #For storing previous loop's error value
+        self.prev_velocity = np.array([0.0, 0.0, 0.0])  #For storing previous loop's velocity value
+        self.integral = np.array([0.0, 0.0, 0.0])       #Integral value storing variable
+        self.derivative = np.array([0.0, 0.0, 0.0])     #Derivative value storing variable
 
-        self.damping = 0.5
-        self.gravity_accel = np.array([0.0, 0.0, -9.81])
+        self.damping = 0.5      #Faking the force felt by plant moving through air with changing velocity
+        self.gravity_accel = np.array([0.0, 0.0, -9.81])    #Gravity constraint
 
-        self.max_integral = 20.0
-        self.max_force = 30.0
+        self.max_integral = 20.0    #Limiting integral value for anti-windup
+        self.max_force = 30.0       #Limiting actuator force for actuator saturation
 
-        self.time = 0.0
+        self.time = 0.0      #total time
 
 
         self.timer = self.create_timer(
